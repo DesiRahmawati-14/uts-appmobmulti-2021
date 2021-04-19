f028e7c78aff69bda2f866850a5423504165c622
Desi Rahmawati <rahmawatidesi730@gmail.com>
2021-04-19 12:16:55 +0700

    mahasiswa dart

---
+++ latihan/lib/Mahasiswa.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+abstract class Mahasiswa {
+    String _nama;
+    int _nrp;
+
+    String getNama() {
+      return _nama;
+    }
+
+    void setNama(String nama) {
+      _nama = nama;
+    }
+
+    int getNrp() {
+      return _nrp;
+    }
+
+    void setNrp(int nrp) {
+      _nrp = nrp;
+    }
+
+    Mahasiswa(this._nama, this._nrp);
+
+    void info();
+}
